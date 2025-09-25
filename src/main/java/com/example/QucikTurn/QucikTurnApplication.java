fe7d58d4721a5ac752e7f88b3e2c78346b3add03
Sakahere4u_ <148515040+SamuelKaunang@users.noreply.github.com>
2025-09-25 18:28:53 +0700

    Initialized base framework

---
+++ src/main/java/com/example/QucikTurn/QucikTurnApplication.java
@@ -0,0 +1,13 @@
+package com.example.QucikTurn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QucikTurnApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QucikTurnApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/QucikTurn/QucikTurnApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.QucikTurn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QucikTurnApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
